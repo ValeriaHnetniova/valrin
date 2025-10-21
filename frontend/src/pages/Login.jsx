be146f002eb1af1855b883ad29dfcf0b33f4bc59
ValeriaHnetniova <hnetnova.valeriia@student.uzhnu.edu.ua>
2025-10-21 08:53:10 +0300

    changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header";
 import Forms from "../components/Forms/Forms";
 import Footer from "../components/Footer/Footer";
 
+
 function Login() {
   return (
     <>
